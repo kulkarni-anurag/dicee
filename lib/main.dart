af52f9220baf312df2082256da87b6a09076502d
kulkarni-anurag <kanurag0801@gmail.com>
2021-08-11 17:24:33 +0530

    Image.asset used

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,10 @@ class DicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(children: <Widget>[
       Expanded(
-        flex: 2,
-        child: Image(
-          image: AssetImage('images/dice1.png'),
-        ),
+        child: Image.asset('images/dice1.png'),
       ),
       Expanded(
-        flex: 1,
-        child: Image(
-          image: AssetImage('images/dice2.png'),
-        ),
+        child: Image.asset('images/dice2.png'),
       ),
     ]);
   }
